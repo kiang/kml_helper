c6903d3bb0767bf94bb53f03d4348bf0016843c4
kiang <kiange@gmail.com>
2019-07-09 10:47:18 +0800

    first draft

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+$(function() {
+  $('#btnSubmit').click(function() {
+    var numMeter = parseInt($('#textMeters').val());
+    var lines = $('#textPoints').val().split(/\r?\n/);
+    var xmlResult = '';
+    for(k in lines) {
+      var p = lines[k].split(',');
+      xmlResult += kml_ring_with_placeMark(parseFloat(p[1]), parseFloat(p[0]), numMeter, 40, 0, 'point', 'point detail');
+    }
+    download('result.kml', kml_header('病例範圍') + xmlResult + kml_footer());
+    return false;
+  });
+})
